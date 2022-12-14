9afc3e63590b86cde6131f8a7bd8eb6cc1a02d51
Jim Segal <hello@jimsegal.com>
2022-12-14 11:23:30 -0600

    Add testing framework
    npm i -D vitest
    update node engine to 18.4.0
    add base test
    add test command

---
+++ src/pages/landing/index.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+import Landing from "./index";
+
+describe("<Landing>", () => {
+  it("renders Landing header", () => {
+    render(<Landing />);
+    const linkElement = screen.getByText(/Landing/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
