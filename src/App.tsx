7da88441c89c18f1a8954b275e2ecb849b425aae
Jim Segal <hello@jimsegal.com>
2022-12-14 10:42:38 -0600

    add landing component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Landing from "./pages/landing";
 
 function App() {
   return (
     <div className="App">
       <h1>JimSegal.com</h1>
+      <Landing />
     </div>
   );
 }
---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,5 @@
+const Landing = () => {
+  return <div>LANDING</div>;
+};
+
+export default Landing;
